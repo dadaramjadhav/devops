6455e9f5aa7c6c25cc6745a766896e62e6265007
dadaramjadhav <dadaramjadhav@gmail.com>
2024-05-01 16:11:59 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install docker.io -y
-sudo usermod -aG docker $USER   #my case is ubuntu
+sudo usermod -aG docker $USER   
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
